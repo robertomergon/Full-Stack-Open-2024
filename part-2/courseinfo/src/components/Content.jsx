4955417b7bc96056369b819a272e46b3e3cbc46e
robertomergon <92520941+robertomergon@users.noreply.github.com>
2024-09-16 14:36:44 +0200

    [FEAT] Exercices from 2.1 to 2.5

---
+++ part-2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,11 @@
+import Part from './Part'
+
+const Content = ({ parts }) => {
+    return (
+        <div>
+            {parts.map((part, id) => <Part key={id} part={part} />)}
+        </div>
+    )
+}
+
+export default Content
