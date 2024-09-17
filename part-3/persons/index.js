955eec3b15849fabc77cbde564beb3cd452014f4
Roberto <92520941+robertomergon@users.noreply.github.com>
2024-09-17 17:45:59 +0200

    [FEAT] Exercises 3.1 -3.6

---
+++ part-3/persons/index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+
+let persons = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>');
+});
+
+// Get the info of the whole phonebook
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+});
+
+// Get the info of the length of the phonebook and the current date
+app.get('/info', (req, res) => {
+    const date = new Date();
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`);
+});
+
+// Get the info of a single person by id
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(person => person.id === id);
+    if (person) {
+        res.json(person);
+    } else {
+        res.status(404).end();
+    }
+});
+
+// Delete a person by id
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    persons = persons.filter(person => person.id !== id);
+    res.status(204).end();
+});
+
+function getLastId() {
+    const ids = persons.map(person => person.id);
+    return Math.max(...ids);
+}
+
+// Add a new person to the phonebook
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    if (!body.name || !body.number) {
+        return res.status(400).json({
+            error: 'name or number is missing'
+        });
+    }
+    if (persons.find(person => person.name === body.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        });
+    }
+    const person = {
+        id: getLastId() + 1,
+        name: body.name,
+        number: body.number
+    };
+    persons = persons.concat(person);
+    res.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
