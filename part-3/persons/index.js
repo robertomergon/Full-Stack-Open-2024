943b7c4677fcf01f7c01c380194b4a0072ac0fc8
robertomergon <92520941+robertomergon@users.noreply.github.com>
2024-09-19 14:14:41 +0200

    [FIX] Minor fixes on validation

--- part-3/persons/index.js
+++ part-3/persons/index.js
@@ -13,14 +13,22 @@ app.use(express.json());
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-const errorHandler = (error, req, res, next) => {
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
     if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' });
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message });
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (
+        error.name === 'ValidationError' ||
+        error.number === 'ValidationError'
+    ) {
+        return response.status(400).json({
+            error: error.message,
+        })
     }
-    next(error);
-};
+
+    next(error)
+}
 
 app.use(errorHandler);
 
@@ -94,7 +102,6 @@ app.post('/api/persons', (req, res, next) => {
     Person.create(person).then(newPerson => {
         res.json(newPerson);
     }).catch(error => {
-        console.log("HOLA", error.name);
         next(error);
     });
 });
