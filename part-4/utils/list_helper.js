441ced68953f627593a05d672dad4b637ad3f892
robertomergon <92520941+robertomergon@users.noreply.github.com>
2024-10-15 22:28:14 +0200

    4.5

--- part-4/utils/list_helper.js
+++ part-4/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((favorite, blog) => favorite.likes > blog.likes ? favorite : blog, blogs[0])
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
