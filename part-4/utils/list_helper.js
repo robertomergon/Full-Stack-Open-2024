d7a734502a3eccd9fc47e40289357c27e6364fc7
robertomergon <92520941+robertomergon@users.noreply.github.com>
2024-10-15 22:51:44 +0200

    4.6 and 4.7

--- part-4/utils/list_helper.js
+++ part-4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const collection = require('lodash/collection')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -10,8 +12,32 @@ const favoriteBlog = (blogs) => {
     return blogs.reduce((favorite, blog) => favorite.likes > blog.likes ? favorite : blog, blogs[0])
 }
 
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) return null
+    const authorCounts = collection.countBy(blogs, 'author');
+    const authors = Object.keys(authorCounts);
+    const authorWithMostBlogs = authors.reduce((max, author) => authorCounts[max] > authorCounts[author] ? max : author, authors[0]);
+    return {
+        author: authorWithMostBlogs,
+        blogs: authorCounts[authorWithMostBlogs]
+    };
+}
+
+const mostLikes = (blogs) => {
+    if (blogs.length === 0) return null
+    const likesByAuthor = collection.groupBy(blogs, 'author');
+    const authors = Object.keys(likesByAuthor);
+    const authorWithMostLikes = authors.reduce((max, author) => totalLikes(likesByAuthor[max]) > totalLikes(likesByAuthor[author]) ? max : author, authors[0]);
+    return {
+        author: authorWithMostLikes,
+        likes: totalLikes(likesByAuthor[authorWithMostLikes])
+    };
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
