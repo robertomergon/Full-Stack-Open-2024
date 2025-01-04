8981f15d7c49e35f7fa8467d9bc4360765343f4f
robertomergon <92520941+robertomergon@users.noreply.github.com>
2025-01-04 23:41:52 +0100

    5.7 Toggle details of the blogs

--- part-5/bloglist-frontend/src/components/Blog.jsx
+++ part-5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import Togglable from "./Toggable"
+import { useRef } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogRef = useRef()
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <Togglable buttonLabel='view' cancelLabel='hide' ref={blogRef}>
+        <div>
+          {blog.url}
+          <br />
+          {blog.likes} likes <button>like</button>
+          <br />
+          added by {blog.user.name}
+        </div>
+      </Togglable>
+    </div>
+  )
+}
 
 export default Blog
